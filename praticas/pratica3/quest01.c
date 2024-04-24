11802e92800da7a8ac48b098c24a1f9234c84693
amanicieslak <160738333+amanicieslak@users.noreply.github.com>
2024-04-24 13:52:33 +0000

    aula 07

---
+++ praticas/pratica3/quest01.c
@@ -0,0 +1,28 @@
+/*
+1. Faça um programa em C que determine se um número inteiro lido é par ou ímpar.
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+int numero;
+printf("Digite um numero inteiro: ");
+  int deu_certo = scanf("%i", &numero);
+
+  if (deu_certo){
+int numero_eh_divisivel_por_2 = numero % 2 == 0;
+
+  if (numero_eh_divisivel_por_2){
+printf("O numero %i é par\n", numero);
+  }
+  else {
+  printf("O numero %i é impar\n", numero);
+  }
+  }
+  else {
+    printf("Não é um numero valido\n");
+  }
+  
+return 0;
+}
